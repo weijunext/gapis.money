0e716e455beb841db528b9b35eac9b103ee6477f
WEI JUN <weijunext@gmail.com>
2024-05-21 09:18:00 +0800

    feat: remove sitecard

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
     <div className="flex flex-row w-full pt-6">
       <div className="hidden md:block md:w-1/5 pl-6"></div>
       <div className="w-full md:w-3/5 px-6">
-        <SiteCard />
+        {/* <SiteCard /> */}
         <WeeklyList posts={posts} />
         <DeveloperCard />
       </div>
