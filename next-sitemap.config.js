a91525e777bb0d83d8131c49eae270a9775d10c7
WEI JUN <weijunext@gmail.com>
2024-07-02 09:08:43 +0800

    feat: sitemap & robots exclude webp

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,16 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://gapis.money",
   generateRobotsTxt: true,
   sitemapSize: 7000,
+  exclude: [
+    "**/*.webp", // 这将排除所有以 .webp 结尾的URL
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/", // 允许爬虫访问所有内容
+        disallow: ["/assets/"], // 所有.webp文件存储在/assets/目录
+      },
+    ],
+  },
 };
